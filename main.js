9bf8d25eba71985ae7f47480851a135074a37c0b
vincentjarrige <jarrige.v@gmail.com>
2025-05-18 14:04:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -78,8 +78,8 @@ const broadcast = (ws, message, includeSelf) => {
 };
 
 
-//app.get('/', (req, res) => {
-//    res.send('Hello World!');
-//});
-app.use(express.static("public"));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+//app.use(express.static("public"));
 
